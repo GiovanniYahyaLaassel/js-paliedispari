6db88b429a2f5ac4f7929beb86a2d06cc946e052
Laassel <lassel.giovanni@gmail.com>
2024-10-18 13:44:26 +0200

    add variables and function to ask the user

---
+++ Palidroma/script.js
@@ -0,0 +1,14 @@
+//Ex-Palidroma
+
+// Chiedere all’utente di inserire una parola 
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// Chiedo all'utente di inserire una parola
+const insertWord = prompt('Inserisci una parola');
+
+//Creo una funzione che determina se l'utente inserirà la parola palindroma
+function isWord(string) {
+
+}
+
+const word = isWord(stringResult);
