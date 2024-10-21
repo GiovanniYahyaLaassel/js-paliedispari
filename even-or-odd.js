5caa028de393b4f8b65ac458c6691c65a9c30113
Laassel <lassel.giovanni@gmail.com>
2024-10-21 10:17:03 +0200

    first-commit in class

---
+++ even-or-odd.js
@@ -0,0 +1,14 @@
+console.log('Pari e dispari');
+
+// Pari e Dispari 
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. 
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). 
+// Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+
+
+// Chiediamo all'utente di inserire 'pari' o 'dispari'.
+const sceltaUtente  =prompt('Scrivi pari o dispari');
+// chiediamo un numero da 1 a 5
+const numeroUtente = parseInt(prompt('Inserisci un numero da 1 a 5'));
+console.log(sceltaUtente, numeroUtente);
+
